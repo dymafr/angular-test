c1a2eaf993db54effc4f5ba7ae54c2f354e7c7bf
Erwan Vallée <erwanp.vallee@gmail.com>
2024-07-26 14:20:40 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-test';
+  counter: number = 0;
+  private subscription!: Subscription;
+
+  ngOnInit() {
+    this.subscription = interval(1000).subscribe(() => {
+      this.counter++;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
